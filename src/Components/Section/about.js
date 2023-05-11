6707827f6758a3ac5018d40e6a33dda2e29a3c57
Aakash <akashsonawane152256@gmail.com>
2023-05-11 15:44:02 +0530

    last

--- src/Components/Section/about.js
+++ src/Components/Section/about.js
@@ -3,12 +3,12 @@ const About=()=>{
         <div style={{margin:"20px"}}>
           <div>
           <h5>How to use</h5>
-          <p>click on <img src="./plus.png" width={`${23}px`} height={`${23}px`}/> after clicking it shows a madal in which there are two field <b>title</b> and <b>Note</b></p>
-          <p><img src='./addNote.png' width={`${70}%`} height={`${280}px`}></img></p>
+          <p>click on <img src="./plus.png" width={`${23}px`} height={`${23}px`} alt="plus button"/> after clicking it shows a modal in which there are two field <b>title</b> and <b>Note</b></p>
+          <p><img src='./addNote.png' width={`${70}%`} height={`${280}px`} alt="screen"></img></p>
           <p>click on <b>add button</b> after that i will save it into notes</p>
           <p>to view notes click on <b>Notes</b>
           </p>
-          <div><img src="./appview.png" width={`${80}%`} height={`${350}px`}></img></div>
+          <div><img src="./appview.png" width={`${80}%`} height={`${350}px`} alt="app view"></img></div>
           </div>
         </div>
     )
